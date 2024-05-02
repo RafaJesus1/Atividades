9cc6f17f523de39aa7adf8ac4510883b8d4f8d8d
bita <167823565+RafaJesus1@users.noreply.github.com>
2024-05-01 22:30:09 -0300

    Atividade02
    Fazer um algoritmo para ler dois vetores V1 e V2 de 15 números cada. Calcular e escrever a
    quantidade de vezes que V1 e V2 possuem os mesmos números e nas mesmas posições.

---
+++ Atividade02.py
@@ -0,0 +1,28 @@
+import random 
+
+'''
+Faça um algoritmo para ler dois vetores V1 e V2 de 15 números cada. Calcular e escrever a
+quantidade de vezes que V1 e V2 possuem os mesmos números e nas mesmas posições.
+'''
+
+Vetor1 = []
+Vetor2 = []
+
+while len(Vetor1) < 15 and len(Vetor2) < 15:
+    num_vetor1 = random.randint(1, 15)
+    Vetor1.append(num_vetor1)
+
+    num_vetor2 = random.randint(1, 15)
+    Vetor2.append(num_vetor2)
+
+qntd_num_repetidos = 0
+posicao = 0
+for i in (range(len(Vetor1)) and range(len(Vetor2))):
+    posicao += 1
+    if Vetor1[i] in Vetor2:
+        if Vetor1[i] == Vetor2[i]:
+            qntd_num_repetidos += 1
+            print(f"Os números {Vetor1[i]} e {Vetor2[i]} são iguais e estão na mesma posição {posicao}")
+            
+print(f"Os vetores 1 e 2 tem exatamente {qntd_num_repetidos} números repetidos.")
+
