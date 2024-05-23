88473fdded54215e358823587cb4e72ca2867959
Rafael <167823565+RafaJesus1@users.noreply.github.com>
2024-05-23 09:09:54 -0300

    Calculadora usando lambda
    Fiz algumas alterações em relação ao exercício anterior

--- Aula22.py
+++ Aula22.py
@@ -1,10 +1,23 @@
 
+from ntpath import join
+
+def calculo():
+    if simbolo == "+":
+        print(f"Resultado: {soma(a, b)}")
+    if simbolo == "-":
+        print(f"Resultado: {subt(a, b)}")
+    if simbolo == "/":
+        print(f"Resultado: {divi(a, b)}")
+    if simbolo == "*":
+        print(f"Resultado: {mult(a, b)}")
+
 soma = lambda a, b: a + b
 mult = lambda a, b: a * b
 subt = lambda a, b: a - b
 divi = lambda a, b: a / b 
 
-simbolos = ("+", "-", "/", "*")
+simbolos = "+", "-", "/", "*"
+ 
 
 while True:
     a = int(input("Digite o valor de A: "))
@@ -13,14 +26,28 @@ while True:
     simbolo = input("Digite o símbolo matemático: ")
     while simbolo not in simbolos:
         print("Tente novamente")
-        print(f"Os símbolos matemáticos são: {simbolos}")
+        print(f"Os símbolos matemáticos são: {" | ".join(simbolos)}")
         simbolo = input("Digite o símbolo matemático: ")
 
-    if simbolo == "+":
-        print(soma(a, b))
-    if simbolo == "-":
-        print(subt(a, b))
-    if simbolo == "/":
-        print(divi(a, b))
-    if simbolo == "*":
-        print(mult(a, b))
+    calculo()
+    
+    print("Deseja continuar? 1 para SIM e 0 para NÃO")
+    opcao = int(input())
+
+    while opcao != 1 or 0:
+        print("Opção inválida.")
+        print("Deseja continuar? 1 para SIM e 0 para NÃO")
+        opcao = int(input())
+
+    if opcao == 1:
+        print("Faça uma nova operação")
+    if opcao == 0:
+        print("Saindo...")
+        break
+
+
+    
+
+
+
+
