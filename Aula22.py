0fb9b8c04a46119a055b7169cbe454edfde3fc80
Rafael <167823565+RafaJesus1@users.noreply.github.com>
2024-05-27 08:36:53 -0300

    Aula22.py

--- Aula22.py
+++ Aula22.py
@@ -10,14 +10,17 @@ def calculo():
         print(f"Resultado: {divi(a, b)}")
     if simbolo == "*":
         print(f"Resultado: {mult(a, b)}")
+    if simbolo == "**":
+        print(f"Resultado: {pont(a, b)}")
 
 soma = lambda a, b: a + b
 mult = lambda a, b: a * b
 subt = lambda a, b: a - b
-divi = lambda a, b: a / b 
+divi = lambda a, b: a / b
+pont = lambda a, b: a ** b
 
-simbolos = "+", "-", "/", "*"
- 
+simbolos = "+", "-", "/", "*", "**"
+opcoes = (0, 1)
 
 while True:
     a = int(input("Digite o valor de A: "))
@@ -34,7 +37,7 @@ while True:
     print("Deseja continuar? 1 para SIM e 0 para NÃO")
     opcao = int(input())
 
-    while opcao != 1 or 0:
+    while opcao not in opcoes:
         print("Opção inválida.")
         print("Deseja continuar? 1 para SIM e 0 para NÃO")
         opcao = int(input())
